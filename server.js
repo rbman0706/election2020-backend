b5ea97e37661bca3f8c8a89dd727d8adb2323447
rbman0706 <brendanshultz@gmail.com>
2020-09-08 17:39:45 -0500

    starting

---
+++ server.js
@@ -0,0 +1,19 @@
+// imports
+const express = require('express');
+const routes = require('./routes');
+// const cors = require('cors')
+
+const port = process.env.PORT || 3001;
+const app = express();
+
+// middleware - JSON parsing
+app.use(express.json())
+
+// middleware - cors
+app.use(cors())
+
+// middleware - API routes
+app.use('/api/election', routes.localtion);
+
+// connection
+app.listen(port, () => console.log(`Server is running on port ${port}`));
